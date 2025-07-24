38ab27f03560d4fbadab6ec3a26c79fd95ca69fb
Tejeswar001 <tejeswarnaidu9@gmail.com>
2025-07-24 20:01:54 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,18 @@ import {connectDB} from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
-app.use(cors({
-    origin: "http://localhost:5173", // Adjust this to your frontend URL
-}))
+if(!process.env.NODE_ENV === "production") {
+    app.use(cors({
+        origin: "http://localhost:5173", // Adjust this to your frontend URL
+    }))
+}
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -25,11 +29,18 @@ app.use(rateLimiter)
 
 app.use("/api/notes",notesRoutes);
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    });
+}
+
+
 
 connectDB().then(()=>{
     app.listen(5001,()=>{
         console.log("Server is running on port 5001");
     });
 });
-
-
